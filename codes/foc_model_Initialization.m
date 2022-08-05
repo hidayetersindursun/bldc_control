dc7618849023d5ada9e010eb5cfe044e194e18b0
Hidayet <h.e.dursun@gmail.com>
2022-08-05 14:06:51 +0300

    foc model edited

--- codes/foc_model_Initialization.m
+++ codes/foc_model_Initialization.m
@@ -10,8 +10,8 @@ Lambda_m = 0.2205;
 Polepairs = 4;
 
 %% Controller parameters
-Iq_reference = 2; % defalult is 100
-Id_reference = 0; % defalult is -50
-Gain_P = 7;  %% default is 1
-Gain_I = 26; %% default is 1
+Iq_reference = 100; % defalult is 100
+Id_reference = -50; % defalult is -50
+Gain_P = 1;  %% default is 1
+Gain_I = 1; %% default is 1
 
