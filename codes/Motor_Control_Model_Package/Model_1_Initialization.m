cb39ae2ddb9dd49b26b9b4f29c0c228f4e15f8bf
Hidayet <h.e.dursun@gmail.com>
2022-08-05 11:40:13 +0300

    model1 and model2 understood

--- codes/Motor_Control_Model_Package/Model_1_Initialization.m
+++ codes/Motor_Control_Model_Package/Model_1_Initialization.m
@@ -10,7 +10,7 @@ Lambda_m = 0.2205;
 Polepairs = 4;
 
 %% Controller parameters
-Iq_reference = 100;
+Iq_reference = 500;
 Id_reference = -50;
 Gain_P = 7;  %% default is 1
 Gain_I = 26; %% default is 1
