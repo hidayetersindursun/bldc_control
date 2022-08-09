184bfbdb972464075d8d16d1023fce9718f414d5
Hidayet <h.e.dursun@gmail.com>
2022-08-09 16:50:29 +0300

    SVPWM added

--- codes/foc_model_Initialization.m
+++ codes/foc_model_Initialization.m
@@ -3,16 +3,15 @@ clear all;
 bdclose all;
 clc;
 %% PMSM parameters
-Ld = 330e-6; % def. :1.7e-3 (Henry)
-Lq = 330e-6; % def. : 3.2e-3 (Henry)
-La = 330e-6; % ?? (Henry)
-Rs = 0.74; % default : 0.02 (Ohm)
-Lambda_m = 330e-6; %flux linkage : default 0.2205
-Polepairs = 7; %default : 4
+Ld = 1.7e-3;
+Lq = 3.2e-3;
+Rs = 0.02;
+Lambda_m = 0.2205;
+Polepairs = 4;
 
 %% Controller parameters
-Iq_reference = 2; % defalult is 100 (A)
-Id_reference = 0; % defalult is -50 (A)
-Gain_P = 1;  %% default is 1
-Gain_I = 1; %% default is 1
+Iq_reference = 100;
+Id_reference = -50;
+Gain_P = 7;
+Gain_I = 26;
 
