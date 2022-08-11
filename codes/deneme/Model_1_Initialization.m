97f64936a46be44bce37a7e2ec229b81c109340d
Hidayet <h.e.dursun@gmail.com>
2022-08-11 15:05:39 +0300

    hız kontrolcüsü eklendi deneme klasörü kullanılıyor

--- codes/deneme/Model_1_Initialization.m
+++ codes/deneme/Model_1_Initialization.m
@@ -7,11 +7,23 @@ Ld = 1.7e-3;
 Lq = 3.2e-3;
 Rs = 0.02;
 Lambda_m = 0.2205;
-Polepairs = 4;
+Polepairs = 7;
 
 %% Controller parameters
 Iq_reference = 100;
-Id_reference = -50;
-Gain_P = 1;
-Gain_I = 1;
+Id_reference = 0;
+Gain_P = 7;
+Gain_I = 26;
+Speed_Gain_P = 0.1;
+Speed_Gain_I = 0.1;
 
+%% Alpha-Beta test
+for th = [0 90 180 270 360]
+    %x = [0;0];
+    A = [cosd(th) -sind(th); sind(th) cosd(th)];
+    B = [Id_reference;Iq_reference];
+    x = A*B;
+    disp(th);
+    disp(x);
+end
+    
