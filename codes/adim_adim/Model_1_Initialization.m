9b42ca494de2349f3add8ca0bd7d1e0ce3e33b34
Hidayet <h.e.dursun@gmail.com>
2022-08-24 15:53:29 +0300

    son : blank model.slx ve init dosyası
    blank model şu an iq_feedbacki doğru veriyor fakat vphmax ve PI kontrolcüsünün sebep olduğu bi hatadan dolayı 0.8-0.9. saniyelerde vd ve  vq saçmalıyor. bunun sonucu olarak da id-iq ve iabc de etklieniyor. Çözümü aranıyor.

---
+++ codes/adim_adim/Model_1_Initialization.m
@@ -0,0 +1,35 @@
+%% Copyright 2018 The MathWorks, Inc.%%
+clear all;
+bdclose all;
+clc;
+
+%% PMSM parameters
+Ld = 0.0002;  % D-axis inductance
+Lq = 0.0002;  % Q-axis inductance
+Rs = 0.013;
+Lambda_m = 0.03;
+PM = Lambda_m;
+Polepairs = 6;
+N = Polepairs;
+L0 = 0.00018; % Zero-sequence inductance
+J = 0.2;
+
+%% Time constants
+Tsc = 5e-5;
+Ts = 2e-6;
+fsw = 2000;
+
+%% Controller parameters
+Iq_reference = 100;
+Id_reference = 0;
+Gain_P = 0.1; 
+Gain_I = 10; 
+Gain_Windup = 1000;
+Speed_Gain_P = 0.1;
+Speed_Gain_I = 0.1;
+
+Pmax = 2.827433388230814e+03;
+Tmax = 27;
+
+    
+
--- codes/eski modele yeni motor/Model_1_Initialization.m
+++ codes/eski modele yeni motor/Model_1_Initialization.m
@@ -11,15 +11,20 @@ Lambda_m = 0.2205;
 Polepairs = 4;
 N = Polepairs;
 L0 = 1.7e-3; % Zero-sequence inductance
- 
+J = 0.2;
+
 %% Controller parameters
 Iq_reference = 100;
 Id_reference = 0;
-Gain_P = 7;
-Gain_I = 26;
+Gain_P = 0.1; 
+Gain_I = 10; 
+Gain_Windup = 1000;
 Speed_Gain_P = 0.1;
 Speed_Gain_I = 0.1;
 
+Tsc = 5e-5;
+Ts = 2e-6;
+fsw = 2000;
 
     
 
