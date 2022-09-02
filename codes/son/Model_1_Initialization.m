0f9db6157089ed9703c8d0b265db7ef69e69b1cd
Hidayet <h.e.dursun@gmail.com>
2022-09-02 14:23:05 +0300

    blank_model.slx ve init.m düzenlendi
    motor parametreleri değişkene atandı

--- codes/son/Model_1_Initialization.m
+++ codes/son/Model_1_Initialization.m
@@ -4,15 +4,14 @@ bdclose all;
 clc;
 
 %% PMSM parameters
-%Ld = 0.0002;  % D-axis inductance
-%Lq = 0.0002;  % Q-axis inductance
-%Rs = 0.013;
-%Lambda_m = 0.03;
-%PM = Lambda_m;
+Ld = 330e-6;  % H
+Lq = 330e-6;  % H
+Rs = 0.03;   % Ohm
 Polepairs = 7;
 N = Polepairs;
-%L0 = 0.00018; % Zero-sequence inductance
-%J = 0.2;
+J = 2.31e-3;   % kg*m^2
+kTorque = 14.7;   % mN*m/A
+PM = 0.03;
 
 %% Time constants
 Tsc = 5e-5;
@@ -20,14 +19,14 @@ Ts = 2e-6;
 fsw = 2000;
 
 %% Controller parameters
-Iq_reference = 100;
+%Iq_reference = 100;
 Id_reference = 0;
-Gain_P = 0.1; 
-Gain_I = 10; 
-Gain_Windup = 1000;
+%Gain_P = 0.1; 
+%Gain_I = 10; 
+%Gain_Windup = 1000;
 
-Speed_Gain_P = 10;
-Speed_Gain_I = 0.2;
+%Speed_Gain_P = 10;
+%Speed_Gain_I = 0.2;
 
 %Pmax = 2.827433388230814e+03;
 %Tmax = 27;
